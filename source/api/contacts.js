da10223e1f724adcbf22947b323143bf91fa3eac
alexander.beletsky <alexander.beletsky@gmail.com>
2013-04-26 10:29:22 +0300

    contacts application added

---
+++ source/api/contacts.js
@@ -0,0 +1,23 @@
+module.exports = function (app) {
+	var container = [{
+			firstName: 'Alexander',
+			lastName: 'Beletsky',
+			email: 'alexander.beletsky@gmail.com',
+			phoneNumber: '055 555-5555'
+		},{
+			firstName: 'John',
+			lastName: 'Smith',
+			email: 'jsmith@gmail.com',
+			phoneNumber: '055 555-5556'
+		},{
+			firstName: 'Marry',
+			lastName: 'Jane',
+			email: 'marry.jane@gmail.com',
+			phoneNumber: '055 555-5557'
+		}
+	];
+
+	app.get('/api/contacts', function (req, res) {
+		res.json(container);
+	});
+};
